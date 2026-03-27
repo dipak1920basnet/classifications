dc9f49d3ae9909feb3b34572c256e512fcf1de61
dipak1920basnet <dipak1920basnet@gmail.com>
2026-03-27 19:29:17 +0545

    apply grid search cv in sklearn

--- gridsearch_cv.py
+++ gridsearch_cv.py
@@ -4,3 +4,15 @@ from sklearn.model_selection import GridSearchCV
 import numpy as np 
 
 X, y = make_classification(n_samples=1000, n_features=20, n_informative=10, n_classes=2, random_state=42)
+
+c_space = np.logspace(-5,8,15)
+
+param_grid = {"C":c_space}
+
+logreg = LogisticRegression()
+logreg_cv = GridSearchCV(logreg, param_grid, cv=5)
+
+logreg_cv.fit(X,y)
+
+print("Tuned Logistic Regression Parameters: {}".format(logreg_cv.best_params_))
+print("Best score is {}".format(logreg_cv.best_score_))
